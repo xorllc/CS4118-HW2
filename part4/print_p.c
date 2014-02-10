9f6bfaa203e94548e933307e4a4bc35955328eb9
josh <joshuaitwaru@gmail.com>
2014-02-10 14:57:22 -0500

    doesn't work...fixing with Huan

--- part4/print_p.c
+++ part4/print_p.c
@@ -3,12 +3,10 @@
 #include <linux/kernel.h>
 #include <linux/list.h>
 #include <linux/slab.h>
-#include <linux/mm.h>
 #include <linux/sched.h>
 
 struct task_struct *t;
 struct list_head *lh;
-int i;
 	
 /*void dfs_print(struct list_head *list, struct task_struct task){
 	list_for_each(list, &((&task)->children)){
@@ -19,6 +17,12 @@ int i;
 }*/
 
 int dummy_fun(struct list_head *l, struct task_struct tk, struct task_struct *tp){
+	//this is fine	
+	/*for_each_process(tp){
+		printk(KERN_INFO "task pid: %i\n", tp->pid);
+	}*/	
+
+	//this generates a warning for frame size
 	list_for_each(l, &((&tk)->children)){
 		tp = list_entry(l, struct task_struct, sibling);
 		printk(KERN_INFO "task pid: %i\n", tp->pid);
@@ -32,20 +36,31 @@ int simple_init(void)
 {
        printk(KERN_INFO "Loading Module\n");
 	
-	//linearly prints tasks
+	//linearly prints tasks; works fine
 	/*for_each_process(task){
 		printk(KERN_INFO "task pid: %i\n", task->pid);
 	}*/
 
 	//the following code works fine and prints task pid
-	/*list_for_each(lh, &((&init_task)->children)){
+	list_for_each(lh, &((&init_task)->children)){
 		t = list_entry(lh, struct task_struct, sibling);
 		printk(KERN_INFO "task pid: %i\n", t->pid);
-	}*/
+	}
 	
 	//the dummy_fun() produces warning: frame size larger than
 	dummy_fun(lh, init_task, t);
 	
+
+
+	//try to use a stack to do DFS
+/*	while(!stack.empty){
+		list_for_each(lh, &((&init_task)->children)){
+			t = list_entry(lh, struct task_struct, sibling);
+			addToStack(t);
+			printk(KERN_INFO "task pid: %i\n", t->pid);
+		}
+	}*/
+
        return 0;
 }
 
